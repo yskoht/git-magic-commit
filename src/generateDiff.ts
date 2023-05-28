9f552201ecfca30587774f515e673324dda6ebde
yskoht <ysk.oht@gmail.com>
2023-05-29 01:01:47 +0900

    Update generateDiff.ts; add untracked file.
    This is a git status update showing changes to be committed to the 'src/generateDiff.ts' file, and an untracked file not listed. The diff command is used to show the changes between the two versions of the file.

--- src/generateDiff.ts
+++ src/generateDiff.ts
@@ -1,6 +1,7 @@
 import { $, ExecaSyncReturnValue } from "execa";
 
 export async function generateDiff(): Promise<string> {
-	const { stdout: diff }: ExecaSyncReturnValue = await $`git diff --staged`;
+	const { stdout: diff }: ExecaSyncReturnValue =
+		await $`git status --verbose --untracked-files=no`;
 	return diff;
 }
