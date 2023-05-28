8844a6820c85d93e557064a18b9f33ba74c5d05a
yskoht <ysk.oht@gmail.com>
2023-05-29 00:22:53 +0900

    "Fix bug in commit.js"
    This code creates a command line interface (CLI) for a program called "git-magic-commit". It allows users to create a commit message with a maximum of 50 characters. The code also imports a function called "commit" from a file called "commit.js" which is used to commit the message.

---
+++ src/cli.ts
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+import meow from "meow";
+
+import { gitMagicCommit } from "./gitMagicCommit.js";
+
+const cli = meow(
+	`
+	Usage
+	  $ git-magic-commit
+
+	Options
+		--verbose Show verbose output
+`,
+	{
+		importMeta: import.meta,
+		flags: {
+			verbose: {
+				type: "boolean",
+				default: false,
+			},
+		},
+	},
+);
+
+gitMagicCommit({
+	verbose: cli.flags.verbose,
+});
--- src/gitMagicCommit.ts
+++ src/gitMagicCommit.ts
@@ -1,6 +1,3 @@
-const verbose = true;
-//const verbose = false;
-
 import { q } from "./utils.js";
 import { createCommitMessage } from "./createCommitMessage.js";
 import { commit } from "./commit.js";
@@ -10,10 +7,13 @@ const question = q(`
 	Please answer the message only in English, no more than 50 characters.
 `);
 
-async function main() {
+export async function gitMagicCommit({
+	verbose,
+}: {
+	verbose: boolean;
+}) {
 	const commitMessage = await createCommitMessage(question, { verbose });
 	console.log({ commitMessage });
+
 	commit(commitMessage);
 }
-
-main();
